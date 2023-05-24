49a538dd399bac66fbb32a29602f4ee03f32a553
Anurag <anuragguptaa9161@gmail.com>
2023-05-24 23:35:21 +0530

    intial commit

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./post.css";
+import { MoreVert } from "@mui/icons-material";
+
+export default function Post() {
+  return (
+    <>
+      <div className="post">
+        <div className="postWrapper">
+          <div className="postTop">
+            <div className="postTopLeft">
+              <img
+                className="postProfilImg"
+                src="/assets/person/1.jpeg"
+                alt="postProfilImg"
+              />
+              <span className="postUsername">Anurag Gupta</span>
+              <span className="postDate">5 mins ago</span>
+            </div>
+            <div className="postTopRight">
+              <MoreVert />
+            </div>
+          </div>
+          <div className="postCenter">
+            <span className="postText">Hey! Its my first post:)</span>
+            <img className="postImg" src="/assets/post/1.jpeg" alt="postImg" />
+          </div>
+          <div className="postBottom">
+            <div className="postBottomLeft">
+              <img className="likeIcon" src="/assets/like.png" alt="likeIcon" />
+              <img
+                className="likeIcon"
+                src="/assets/heart.png"
+                alt="likeIcon"
+              />
+              <span className="postLikeCounter">30 people like it</span>
+            </div>
+            <div className="postBottomRight">
+              <span className="postCommentText">10 comments</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
