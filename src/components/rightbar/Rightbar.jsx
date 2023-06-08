f5e6fc9578aa308167029a3d84c5d95e05bca928
Anurag <anuragguptaa9161@gmail.com>
2023-06-09 00:00:11 +0530

    changes done for profile page

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,29 +3,110 @@ import "./rightbar.css";
 import { Users } from "../../dummyData";
 import Online from "../online/Online";
 
-export default function Rightbar() {
+export default function Rightbar({ profile }) {
+  const HomeRightBar = () => {
+    return (
+      <>
+        <div className="birthdayContainer">
+          <img
+            src="assets/gift.png"
+            alt="birthdayImg"
+            className="birthdayImg"
+          />
+          <span className="birthdayText">
+            <b>John Carter</b> and <b>3 other friends</b> have their birthday
+            today.
+          </span>
+        </div>
+        <img src="assets/ad.png" alt="rightbarAd" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          {Users.map((u) => (
+            <Online key={u.id} user={u} />
+          ))}
+        </ul>
+      </>
+    );
+  };
+
+  const ProfileRightBar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">User information</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City :</span>
+            <span className="rightbarInfoValue">New York</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From :</span>
+            <span className="rightbarInfoValue">Madrid</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Relationship :</span>
+            <span className="rightbarInfoValue">Single</span>
+          </div>
+        </div>
+        <h4 className="rightbarTitle">User friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/1.jpeg"
+              alt="rightbarFollowingImg"
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/2.jpeg"
+              alt="rightbarFollowingImg"
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/3.jpeg"
+              alt="rightbarFollowingImg"
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/4.jpeg"
+              alt="rightbarFollowingImg"
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/5.jpeg"
+              alt="rightbarFollowingImg"
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/6.jpeg"
+              alt="rightbarFollowingImg"
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+
   return (
     <>
       <div className="rightbar">
         <div className="rightbarWrapper">
-          <div className="birthdayContainer">
-            <img
-              src="assets/gift.png"
-              alt="birthdayImg"
-              className="birthdayImg"
-            />
-            <span className="birthdayText">
-              <b>Sachin Punera</b> and <b>3 other friends</b> have their
-              birthday today.
-            </span>
-          </div>
-          <img src="assets/ad.png" alt="rightbarAd" className="rightbarAd" />
-          <h4 className="rightbarTitle">Online Friends</h4>
-          <ul className="rightbarFriendList">
-            {Users.map((u) => (
-              <Online key={u.id} user={u} />
-            ))}
-          </ul>
+          <ProfileRightBar />
         </div>
       </div>
     </>
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -32,7 +32,7 @@ export default function Profile() {
           </div>
           <div className="profileRightBottom">
             <Feed />
-            <Rightbar />
+            <Rightbar profile />
           </div>
         </div>
       </div>
